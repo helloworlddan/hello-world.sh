834ad7f941dd25cae932ed3a45681c1334c6697a
Daniel Stamer <dan@hello-world.sh>
2020-03-20 10:18:14 +0100

    add transparent HTTP listener
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- infrastructure/proxy.tf
+++ infrastructure/proxy.tf
@@ -1,17 +1,37 @@
-resource "google_compute_global_forwarding_rule" "default" {
+resource "google_compute_global_address" "default" {
   project = local.project
-  name = "${local.prefix}-frontend"
-  target = google_compute_target_https_proxy.default.self_link
-  port_range = "443"
+  name = "${local.prefix}-proxy-ip"
 }
 
-resource "google_compute_target_https_proxy" "default" {
+resource "google_compute_global_forwarding_rule" "default_https" {
+  project = local.project
+  name = "${local.prefix}-https-frontend"
+  target = google_compute_target_https_proxy.default_https.self_link
+  port_range = "443"
+  ip_address = google_compute_global_address.default.address
+}
+
+resource "google_compute_target_https_proxy" "default_https" {
   project = local.project
   name = "${local.prefix}-proxy"
   url_map = google_compute_url_map.default.self_link
   ssl_certificates = [
     google_compute_managed_ssl_certificate.default.self_link
   ]
+}
+
+resource "google_compute_global_forwarding_rule" "default_http" {
+  project = local.project
+  name = "${local.prefix}-http-frontend"
+  target = google_compute_target_http_proxy.default_http.self_link
+  port_range = "80"
+  ip_address = google_compute_global_address.default.address
+}
+
+resource "google_compute_target_http_proxy" "default_http" {
+  project = local.project
+  name = "${local.prefix}-proxy"
+  url_map = google_compute_url_map.default.self_link
 }
 
 resource "google_compute_url_map" "default" {
@@ -28,5 +48,5 @@ resource "google_compute_backend_bucket" "default" {
 }
 
 output "proxy_ip" {
-  value = google_compute_global_forwarding_rule.default.ip_address
+  value = google_compute_global_address.default.address
 }
