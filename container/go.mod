a5bb260a453f6fda8bad21c4d4d94bb0cb1d953a
Daniel Stamer <dan@hello-world.sh>
2020-03-23 18:02:33 +0100

    add tracing
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

---
+++ container/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.14
+
+require (
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.0
+	go.opencensus.io v0.22.3
+)
--- container/server.go
+++ container/server.go
@@ -5,16 +5,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/trace"
 )
 
 func main() {
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: os.Getenv("GOOGLE_CLOUD_PROJECT"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	trace.RegisterExporter(exporter)
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+
 	fs := http.FileServer(http.Dir("./site"))
 	http.Handle("/", fs)
 
 	port := os.Getenv("PORT")
 
 	log.Println(fmt.Sprintf("Listening on :%s...", port))
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
