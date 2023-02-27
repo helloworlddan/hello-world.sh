9e7e5554509a9b1b3a1a1b09519041f6c152ca09
Daniel Stamer <stamer@google.com>
2023-02-27 13:24:04 +0000

    drmatically simplify site
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- Makefile
+++ Makefile
@@ -1,14 +1,4 @@
-local:
-	make -C site local
-
 build:
-	make -C site build
-
-post:
-	make -C site post
-
-publish: build
-	cp -r dist container/site
 	gcloud builds submit --tag="gcr.io/$$(gcloud config get-value project)/hwsh" container/
 	gcloud run deploy --region=europe-west4 --image="gcr.io/$$(gcloud config get-value project)/hwsh" hwsh-blog-service
 
@@ -18,11 +8,4 @@ deploy:
 destroy:
 	make -C infrastructure destroy
 
-clean:
-	make -C site clean
-	make -C infrastructure clean
-	rm -rf dist
-	rm -rf container/site
-
-.PHONY: init local build post publish deploy clean destroy
-
+.PHONY:  deploy destroy build
--- container/server.go
+++ container/server.go
@@ -8,13 +8,31 @@ import (
 )
 
 func main() {
+	http.Handle("/", http.FileServer(http.Dir("./site")))
 
-	fs := http.FileServer(http.Dir("./site"))
-	http.Handle("/", fs)
+	for _, endpoint := range []string{"mail", "post"} {
+		http.HandleFunc(fmt.Sprintf("/%s", endpoint), func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("<head><meta http-equiv=\"refresh\" content=\"0; url=mailto:stamer@google.com\" /></head>"))
+		})
+	}
+
+	for _, endpoint := range []string{"tel", "phone", "call"} {
+		http.Handle(fmt.Sprintf("/%s", endpoint), http.RedirectHandler("tel:+491736548706", http.StatusMovedPermanently))
+	}
+
+	for _, endpoint := range []string{"cal", "gcal", "calendar", "schedule"} {
+		http.Handle(fmt.Sprintf("/%s", endpoint), http.RedirectHandler("https://calendar.app.google/kknKZS8UgJtMDvpq7", http.StatusSeeOther))
+	}
+
+	for _, endpoint := range []string{"git", "github", "code"} {
+		http.Handle(fmt.Sprintf("/%s", endpoint), http.RedirectHandler("https://github.com/helloworlddan", http.StatusSeeOther))
+	}
 
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	log.Println(fmt.Sprintf("Listening on :%s...", port))
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Fatal(err)
